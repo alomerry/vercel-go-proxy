a588c44811cf51f0e15ceea57e35de19f22f60de
Alomerry Wu <alomerry.wu@gmail.com>
2023-05-11 00:22:51 +0800

    chore: refine proxy

--- api/handler.go
+++ api/handler.go
@@ -1,16 +1,13 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%+v\n", r)
 	u, _ := url.Parse("https://api.openai.com/")
 	proxy := httputil.NewSingleHostReverseProxy(u)
-	proxy.Director(r)
-	fmt.Printf("%+v\n", r)
+	proxy.ServeHTTP(w, r)
 }
